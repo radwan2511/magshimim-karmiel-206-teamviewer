be31a0e907c6b27b738b11711bc38bb7d43ac060
Danielvih <Danielvihorev@gmail.com>
2018-11-18 19:26:19 +0200

    basic Client

---
+++ TeamViewer - Client/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace TeamViewer___Client
+{
+
+    public partial class Form1 : Form
+    {
+        public static string MSG { get; set; }
+        public static string RecivedMSG { get; set; }
+        public static TcpClient client;
+        public static NetworkStream clientStream;
+        public static byte[] key { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            Thread c = new Thread(serverConnection);
+            c.Start();
+        }
+
+        private void serverConnection()
+        {
+            try
+            {
+                //start the connection
+                client = new TcpClient();
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(Constants.LOCAL_HOST), Constants.PORT);
+                client.Connect(serverEndPoint);
+                NetworkStream clientStream = client.GetStream();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                Application.Exit();
+            }
+
+        }
+    }
+}
