ec14fcb845dd3e73781737bc1d38c88b9816db55
Dan <Danielvihorev@gmail.com>
2019-03-01 23:33:58 +0200

    files

---
+++ Socket-File-Transfer-master/file transfer/Main.cs
@@ -0,0 +1,549 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using file_transfer;
+using System.Security.Cryptography;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+public partial class Main : Form
+{
+    public static byte[] key;
+    public static byte[] IV;
+
+    public static byte[] firstKey;
+    public static byte[] firstIV;
+    public static byte[] secondKey;
+    public static byte[] secondIV;
+    public static string localIp;
+
+    //encrypt function
+    public static byte[] Encrypt(byte[] bytes)
+    {
+        if (bytes == null || key == null || IV == null || bytes.Length <= 0 || key.Length <= 0 || IV.Length <= 0)
+        {
+            return null;
+        }
+        byte[] data;
+        // create aes object with key and IV
+        using (Aes aes = new AesCryptoServiceProvider())
+        {
+            aes.Key = key;
+            aes.IV = IV;
+
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+
+                cs.Write(bytes, 0, bytes.Length);
+                cs.FlushFinalBlock();
+                data = ms.ToArray();
+                
+            }
+        }
+        return data;
+    }
+
+    //decrypt function
+    public static byte[] Decrypt(byte[] bytes)
+    {
+        if (bytes == null || key == null || IV == null || bytes.Length <= 0 || key.Length <= 0 || IV.Length <= 0)
+        {
+            return null;
+        }
+        byte[] data;
+        // create aes object with key and IV
+        using (Aes aes = new AesCryptoServiceProvider())
+        {
+            aes.Key = key;
+            aes.IV = IV;
+
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write);
+
+                cs.Write(bytes, 0, bytes.Length);
+                //cs.FlushFinalBlock();
+                data = ms.ToArray();
+
+            }
+        }
+        return data;
+    }
+
+
+    //This will hold our listener. We will only need to create one instance of this.
+    private Listener listener;
+    //This will hold our transfer client.
+    private TransferClient transferClient;
+    //This will hold our output folder.
+    private string outputFolder;
+    //This will hold our overall progress timer.
+    private Timer tmrOverallProg;
+    //This is our variable to determine of the server is running or not to accept another connection if our client
+    //Disconnects
+    private bool serverRunning;
+
+    public Main()
+    {
+        InitializeComponent();
+        //Create the listener and register the event.
+        listener = new Listener();
+        listener.Accepted += listener_Accepted;
+
+        //Create the timer and register the event.
+        tmrOverallProg = new Timer();
+        tmrOverallProg.Interval = 1000;
+        tmrOverallProg.Tick += tmrOverallProg_Tick;
+
+        //Set our default output folder.
+        outputFolder = "Transfers";
+
+        //If it does not exist, create it.
+        if (!Directory.Exists(outputFolder))
+        {
+            Directory.CreateDirectory(outputFolder);
+        }
+
+        btnConnect.Click += new EventHandler(btnConnect_Click);
+        btnStartServer.Click += new EventHandler(btnStartServer_Click);
+        btnStopServer.Click += new EventHandler(btnStopServer_Click);
+        btnSendFile.Click += new EventHandler(btnSendFile_Click);
+        btnPauseTransfer.Click += new EventHandler(btnPauseTransfer_Click);
+        btnStopTransfer.Click += new EventHandler(btnStopTransfer_Click);
+        btnOpenDir.Click += new EventHandler(btnOpenDir_Click);
+        btnClearComplete.Click += new EventHandler(btnClearComplete_Click);
+
+        btnStopServer.Enabled = false;
+    }
+
+    protected override void OnFormClosing(FormClosingEventArgs e)
+    {
+        //Deregister all the events from the client if it is connected.
+        deregisterEvents();
+        base.OnFormClosing(e);
+    }
+
+    void tmrOverallProg_Tick(object sender, EventArgs e)
+    {
+        if (transferClient == null)
+            return;
+        //Get and display the overall progress.
+        progressOverall.Value = transferClient.GetOverallProgress();
+    }
+
+    void listener_Accepted(object sender, SocketAcceptedEventArgs e)
+    {
+        if (InvokeRequired)
+        {
+            Invoke(new SocketAcceptedHandler(listener_Accepted), sender, e);
+            return;
+        }
+        
+        //Stop the listener
+        listener.Stop();
+
+        //Create the transfer client based on our newly connected socket.
+        transferClient = new TransferClient(e.Accepted);
+        //Set our output folder.
+        transferClient.OutputFolder = outputFolder;
+        //Register the events.
+        registerEvents();
+        //Run the client
+        transferClient.Run();
+        //Start the progress timer
+        tmrOverallProg.Start();
+        //And set the new connection state.
+        setConnectionStatus(transferClient.EndPoint.Address.ToString());
+    }
+
+    private void btnConnect_Click(object sender, EventArgs e)
+    {
+        if (transferClient == null)
+        {
+            //Create our new transfer client.
+            //And attempt to connect
+            transferClient = new TransferClient();
+            transferClient.Connect(txtCntHost.Text.Trim(), int.Parse(txtCntPort.Text.Trim()), connectCallback);
+            Enabled = false;
+            using (Aes aes = Aes.Create())
+            {
+                aes.GenerateKey();
+                aes.GenerateIV();
+                //key = aes.Key;
+                //IV = aes.IV;
+                firstKey = aes.Key;
+                firstIV = aes.IV;
+            }
+            System.Threading.Thread.Sleep(1000);
+            //transferClient.Send(key);
+            //transferClient.Send(IV);
+            transferClient.Send(firstKey);
+            transferClient.Send(firstIV);
+        }
+        else
+        {
+            //This means we're trying to disconnect.
+            transferClient.Close();
+            transferClient = null;
+        }
+    }
+
+    private void connectCallback(object sender, string error)
+    {
+        if (InvokeRequired)
+        {
+            Invoke(new ConnectCallback(connectCallback), sender, error);
+            return;
+        }
+        //Set the form to enabled.
+        Enabled = true;
+        //If the error is not equal to null, something went wrong.
+        if (error != null)
+        {
+            transferClient.Close();
+            transferClient = null;
+            MessageBox.Show(error, "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+        //Register the events
+        registerEvents();
+        //Set the output folder
+        transferClient.OutputFolder = outputFolder;
+        //Run the client
+        transferClient.Run();
+        //Set the connection status
+        setConnectionStatus(transferClient.EndPoint.Address.ToString());
+        //Start the progress timer.
+        tmrOverallProg.Start();
+        //Set our connect button text to "Disconnect"
+        btnConnect.Text = "Disconnect";
+    }
+
+    private void registerEvents()
+    {
+        transferClient.Complete += transferClient_Complete;
+        transferClient.Disconnected += transferClient_Disconnected;
+        transferClient.ProgressChanged += transferClient_ProgressChanged;
+        transferClient.Queued += transferClient_Queued;
+        transferClient.Stopped += transferClient_Stopped;
+    }
+
+    void transferClient_Stopped(object sender, TransferQueue queue)
+    {
+        if (InvokeRequired)
+        {
+            Invoke(new TransferEventHandler(transferClient_Stopped), sender, queue);
+            return;
+        }
+        //Remove the stopped transfer from view.
+        lstTransfers.Items[queue.ID.ToString()].Remove();
+    }
+
+    void transferClient_Queued(object sender, TransferQueue queue)
+    {
+        if (InvokeRequired)
+        {
+            Invoke(new TransferEventHandler(transferClient_Queued), sender, queue);
+            return;
+        }
+
+        //Create the LVI for the new transfer.
+        ListViewItem i = new ListViewItem();
+         i.Text = queue.ID.ToString();
+        i.SubItems.Add(queue.Filename);
+        //If the type equals download, it will use the string of "Download", if not, it'll use "Upload"
+        i.SubItems.Add(queue.Type == QueueType.Download ? "Download" : "Upload");
+        i.SubItems.Add("0%");
+        i.Tag = queue; //Set the tag to queue so we can grab is easily.
+        i.Name = queue.ID.ToString(); //Set the name of the item to the ID of our transfer for easy access.
+        lstTransfers.Items.Add(i); //Add the item
+        i.EnsureVisible();
+        
+        //If the type is download, let the uploader know we're ready.
+        if (queue.Type == QueueType.Download)
+        {
+            transferClient.StartTransfer(queue);
+        }
+    }
+
+    void transferClient_ProgressChanged(object sender, TransferQueue queue)
+    {
+        if (InvokeRequired)
+        {
+            Invoke(new TransferEventHandler(transferClient_ProgressChanged), sender, queue);
+            return;
+        }
+
+        //Set the progress cell to our current progress.
+        lstTransfers.Items[queue.ID.ToString()].SubItems[3].Text = queue.Progress + "%";
+    }
+
+    void transferClient_Disconnected(object sender, EventArgs e)
+    {
+        if (InvokeRequired)
+        {
+            Invoke(new EventHandler(transferClient_Disconnected), sender, e);
+            return;
+        }
+
+        //Deregister the transfer client events
+        deregisterEvents();
+
+        //Close every transfer
+        foreach (ListViewItem item in lstTransfers.Items)
+        {
+            TransferQueue queue = (TransferQueue)item.Tag;
+            queue.Close();
+        }
+        //Clear the listview
+        lstTransfers.Items.Clear();
+        progressOverall.Value = 0;
+
+        //Set the client to null
+        transferClient = null;
+
+        //Set the connection status to nothing
+        setConnectionStatus("-");
+
+        // restoring the aes encrypt keys to null
+        key = null;
+        IV = null;
+        firstKey = null;
+        firstIV = null;
+        secondKey = null;
+        secondIV = null;
+
+        //If the server is still running, wait for another connection
+        if (serverRunning)
+        {
+            listener.Start(int.Parse(txtServerPort.Text.Trim()));
+            setConnectionStatus("Waiting...");
+        }
+        else //If we connected then disconnected, set the text back to connect.
+        {
+            btnConnect.Text = "Connect";
+        }
+    }
+
+    void transferClient_Complete(object sender, TransferQueue queue)
+    {
+        //This just plays a little sound to let us know a transfer completed.
+        System.Media.SystemSounds.Asterisk.Play();
+    }
+
+    private void deregisterEvents()
+    {
+        if (transferClient == null)
+            return;
+        transferClient.Complete -= transferClient_Complete;
+        transferClient.Disconnected -= transferClient_Disconnected;
+        transferClient.ProgressChanged -= transferClient_ProgressChanged;
+        transferClient.Queued -= transferClient_Queued;
+        transferClient.Stopped -= transferClient_Stopped;
+    }
+
+    private void setConnectionStatus(string connectedTo)
+    {
+        lblConnected.Text = "Connection: " + connectedTo;
+    }
+
+    private void btnStartServer_Click(object sender, EventArgs e)
+    {
+        //We disabled the button, but lets just do a quick check
+        if (serverRunning)
+            return;
+        serverRunning = true;
+        try
+        {
+            //Try to listen on the desired port
+            listener.Start(int.Parse(txtServerPort.Text.Trim()));
+            //Set the connection status to waiting
+            setConnectionStatus("Waiting...");
+            //Enable/Disable the server buttons.
+            btnStartServer.Enabled = false;
+            btnStopServer.Enabled = true;
+        }
+        catch
+        {
+            MessageBox.Show("Unable to listen on port " + txtServerPort.Text, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+    }
+
+    private void btnStopServer_Click(object sender, EventArgs e)
+    {
+        if (!serverRunning)
+            return;
+        //Close the client if its active.
+        if (transferClient != null)
+        {
+            transferClient.Close();
+            //INSERT
+            transferClient = null;
+            //
+        }
+        //Stop the listener
+        listener.Stop();
+        //Stop the timer
+        tmrOverallProg.Stop();
+        //Reset the connection statis
+        setConnectionStatus("-");
+        //Set our variables and enable/disable the buttons.
+        serverRunning = false;
+        btnStartServer.Enabled = true;
+        btnStopServer.Enabled = false;
+    }
+
+    private void btnClearComplete_Click(object sender, EventArgs e)
+    {
+        //Loop and clear all complete or inactive transfers
+        foreach (ListViewItem i in lstTransfers.Items)
+        {
+            TransferQueue queue = (TransferQueue)i.Tag;
+
+            if (queue.Progress == 100 || !queue.Running)
+            {
+                i.Remove();
+            }
+        }
+    }
+
+    private void btnOpenDir_Click(object sender, EventArgs e)
+    {
+        //Get a user defined save directory
+        using (FolderBrowserDialog fb = new FolderBrowserDialog())
+        {
+            if (fb.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                outputFolder = fb.SelectedPath;
+
+                if (transferClient != null)
+                {
+                    transferClient.OutputFolder = outputFolder;
+                }
+
+                txtSaveDir.Text = outputFolder;
+            }
+        }
+    }
+
+    private void btnSendFile_Click(object sender, EventArgs e)
+    {
+        if (transferClient == null)
+            return;
+        //Get the user desired files to send
+        using (OpenFileDialog o = new OpenFileDialog())
+        {
+            o.Filter = "All Files (*.*)|*.*";
+            o.Multiselect = true;
+
+            if (o.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (string file in o.FileNames)
+                {
+                    //check file for virus
+                    bool isInfected = false;
+                    using (MD5 md5 = MD5.Create())
+                    {
+                        //string fileString = BitConverter.ToString(md5.ComputeHash(File.ReadAllBytes(file)).Replace("-", string.Empty).ToLower();
+                        string[] md5signatures = File.ReadAllLines("MD5base.txt");
+                        string fileString = File.ReadAllText(file);
+                        for (int i = 0; i < md5signatures.Length; i++)
+                        {
+
+                            if (fileString.Contains(md5signatures[i]))
+                            {
+                                // "Infected!";
+
+                                MessageBox.Show("File: "+file+"  is Infected!\n(can't send file if it is virus)");
+                                isInfected = true;
+                                i = md5signatures.Length;
+                            }
+                        }
+                        System.Threading.Thread.Sleep(1000);
+                        //else
+                        //{
+                        //   "Clean!";
+                        //   
+                        //}
+                    }
+                    if (!isInfected)
+                    {
+                        transferClient.QueueTransfer(file);
+                        sendStatistic(file);
+                    }
+                }
+            }
+        }
+    }
+
+    public void sendStatistic(string file)
+    {
+        //Start of the statistic
+        string sendersIp = localIp; // the ip of the sender
+        string reciversIp = localIp;// the ip of the reciver
+        string fileType = file.Substring(file.IndexOf("."), file[file.Length]);
+        string time = DateTime.Now.ToShortTimeString(); // the current time 
+        string toSend = Constants.STATISTIC_MSG + sendersIp.Length + sendersIp + reciversIp.Length + reciversIp + fileType.Length + fileType + time.Length + time;
+        byte[] send = Encoding.ASCII.GetBytes(toSend);
+        TransferClient.Send(send);
+        //the end of the statstic 
+    }
+
+    private void btnPauseTransfer_Click(object sender, EventArgs e)
+    {
+        if (transferClient == null)
+            return;
+        //Loop and pause/resume all selected downloads.
+        foreach (ListViewItem i in lstTransfers.SelectedItems)
+        {
+            TransferQueue queue = (TransferQueue)i.Tag;
+            queue.Client.PauseTransfer(queue);
+        }
+    }
+
+    private void btnStopTransfer_Click(object sender, EventArgs e)
+    {
+        if (transferClient == null)
+            return;
+
+        //Loop and stop all selected downloads.
+        foreach (ListViewItem i in lstTransfers.SelectedItems)
+        {
+            TransferQueue queue = (TransferQueue)i.Tag;
+            queue.Client.StopTransfer(queue);
+            i.Remove();
+        }
+
+        progressOverall.Value = 0;
+    }
+
+    private void Main_Load(object sender, EventArgs e)
+    {
+        //txtCntHost
+        IPHostEntry host;
+        host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (IPAddress ip in host.AddressList)
+        {
+            if (ip.AddressFamily.ToString() == "InterNetwork")
+            {
+                localIp = ip.ToString();
+                txtCntHost.Text = localIp;
+            }
+        }
+    }
+}
---
+++ Socket-File-Transfer-master/file transfer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace file_transfer
+{
+    static class Constants // defines
+    {
+        public const string LOCAL_HOST = "127.0.0.1",
+        STATISTIC_MSG = "260";
+    }
+
+    static class Program
+    {
+        //What we are using for ID generation
+        public static Random Rand;
+        [STAThread]
+        static void Main()
+        {
+            Rand = new Random();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
